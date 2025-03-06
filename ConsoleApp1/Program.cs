b98645cd4c54299c0bb0c2d94ee06f4f7c91cca6
AmirZukhurov <zukhurovamir96@gmail.com>
2025-03-06 08:09:53 +0500

    secondcommit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Как тебя зовут? ");
-string userInput = Console.ReadLine();
-Console.WriteLine($"Hello, {userInput}");
+﻿// switch (DayOfWeek.Monday)
+// {
+//     case DayOfWeek.Monday:
+//         break;
+//     case DayOfWeek.Tuesday:
+//         break;
+//     default:
+//         break;
+// }
+// enum WeekDay
+// {
+//     Monday,
+//     Tuesday,
+//     Wednesday,
+//     Thursday,
+//     Friday,
+//     Saturday,
+//     Sunday,
+// };
+
+var numbers = new int[]{1,2,3};
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+
+
+
