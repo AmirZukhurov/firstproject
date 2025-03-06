7edbd3698edd3b9e5108924453b9f5d8553305bc
AmirZukhurov <zukhurovamir96@gmail.com>
2025-03-06 17:59:14 +0500

    SimpleCalc

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,12 +18,97 @@
 //     Sunday,
 // };
 
-var numbers = new int[]{1,2,3};
-for (int i = 0; i < numbers.Length; i++)
+// var numbers = new int[]{1,2,3};
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     Console.WriteLine(numbers[i]);
+// }
+
+using System.Text.RegularExpressions;
+
+while (true)
 {
-    Console.WriteLine(numbers[i]);
+    try
+    {
+        // Console.Write("Enter number a: ");
+        // double a = double.Parse(Console.ReadLine());
+        // Console.Write("Enter operation: ");
+        // string oper = Convert.ToString(Console.ReadLine());
+        // Console.Write("Enter number c: ");
+        // double c = double.Parse(Console.ReadLine());
+        Console.Write("Simple Calc\n\nEnter for calculation:");
+        string input = Console.ReadLine();
+        Regex regex = new Regex(@"(\d+)([+\-*/])(\d+)");
+        Match match = regex.Match(input);
+        double a=0, c=0;
+        string oper = string.Empty;
+        if (match.Success)
+        {
+            a = double.Parse(match.Groups[1].Value);
+            oper = match.Groups[2].Value;
+            c = double.Parse(match.Groups[3].Value);
+        }
+        else
+        {
+            Console.WriteLine("Wrong input");
+        }
+        if (oper == "+")
+        {
+            var res = DoOperation(a, c, Operation.Add);
+            Console.Clear();
+            Console.WriteLine($"{a}+{c}={res}");
+        }
+        else if (oper == "-")
+        {
+            var res =DoOperation(a, c, Operation.Subtract);
+            Console.Clear();
+            Console.WriteLine($"Result: {a}-{c}={res}");
+        }
+        else if (oper == "*"){
+            var res =DoOperation(a, c, Operation.Multiply);
+            Console.Clear();
+            Console.WriteLine($"Result: {a}*{c}={res}");
+        }
+        else if (oper == "/"){
+            var res =DoOperation(a, c, Operation.Divide);
+            Console.Clear();
+            Console.WriteLine($"Ответ: {a}/{c}={res}");
+        }
+        else
+            Console.WriteLine("Invalid operation1!");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
 }
 
+double DoOperation(double x, double y, Operation op)
+{
+    switch (op)
+    {
+        case Operation.Add:
+            return x + y;
+        case Operation.Subtract:
+            return x - y;
+        case Operation.Multiply:
+            return x * y;
+        case Operation.Divide:
+            return x / y;
+        default:
+            Console.WriteLine("Invalid operation2!");
+            break;
+    }
+    return 0;
+}
+enum Operation
+{
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
 
 
 
